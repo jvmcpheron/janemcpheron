0105504b2ed83eed8bf2d61f2ec2f74466b9d0fe
Jane McPheron <mcphe223@d.umn.edu>
2025-03-08 16:10:01 -0600

    made hamburger menu spans change into an X when menu is open

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const themeButton = document.getElementById('theme-button');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
+const span1 = document.querySelector('#span1')
+const span2 = document.querySelector('#span2')
+const span3 = document.querySelector('#span3')
 
 // Default mode to light
 document.body.classList.remove('dark-mode');
@@ -27,6 +30,9 @@ themeButton.addEventListener('click', () => {
 // Toggle hamburger menu
 hamburgerMenu.addEventListener('click', () => {
     menu.classList.toggle('open');
+    span1.classList.toggle('active');
+    span2.classList.toggle('active');
+    span3.classList.toggle('active');
 });
 
 document.addEventListener("DOMContentLoaded", () => {
